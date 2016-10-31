7cc84bc441df484f8149c0be80364b42e08a373a
luthfi <luthfiswees@gmail.com>
2016-10-31 15:36:49 +0700

    change options in migration.js, hope it works

--- config/migration.js
+++ config/migration.js
@@ -2,7 +2,4 @@
 module.exports.migrations = {
   // connection name matches a field from config/connections.js
   connection: 'herokuPostgre', // or MySQL
-  table: 'Data',
-  migrationsDir: 'sails_migrations',
-  coffeeFile: true
 };
